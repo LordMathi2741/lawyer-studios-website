8019a5a5156ae21243fc297b800bfea436e39549
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-07-23 12:29:42 -0500

    style(public/navbar): added hover buttons styles

--- app/public/components/navbar-content.component.tsx
+++ app/public/components/navbar-content.component.tsx
@@ -16,7 +16,7 @@ export default function NavBarContent(){
               <Image src="/assets/logo-final-transparente2.jpg" alt="Brand logo" width={100} height={30}/>
             </div>
             <div className=" flex flex-col">
-                <RxHamburgerMenu onClick={setToggleToolbar} className={`${!toolbar ? 'block' :'hidden' } text-xl lg:hidden`}/>
+                <RxHamburgerMenu onClick={setToggleToolbar} className={`${!toolbar ? 'block' :'hidden' } hover:text-yellow-600 hover:cursor-pointer text-xl lg:hidden`}/>
                 <ul className={`${toolbar ? 'flex':'hidden'}   text-center lg:flex flex-col lg:flex-row gap-2 lg:gap-5`}>
                     <li className="hover:bg-yellow-600 hover:cursor-pointer lg:p-3 xl:p-5 "> Inicio </li>
                     <li className="hover:bg-yellow-600 hover:cursor-pointer lg:p-3 xl:p-5"> Nosotros </li>
@@ -29,7 +29,7 @@ export default function NavBarContent(){
                         <p className="lg:text-sm xl:text-xl"> +51 974 660 869</p>
                     </li>
                     <li className="mx-auto">
-                        <IoMdCloseCircleOutline onClick={setToggleToolbar} className="text-xl lg:hidden"/>
+                        <IoMdCloseCircleOutline onClick={setToggleToolbar} className="text-xl lg:hidden hover:text-yellow-600 hover:cursor-pointer"/>
                     </li>
                 </ul>
             </div>
